6b17a52ed54cd47b5467916d4035a219681aee2f
Jens Caenen <Jens.Caenen@student.pxl.be>
2020-03-18 17:13:55 +0100

    Updated constructor

--- src/util/Date.php
+++ src/util/Date.php
@@ -11,21 +11,9 @@ class Date
 
     private function __construct($day = 1, $month = 1, $year = 2008)
     {
-        $arguments = func_num_args();
-        if ($arguments == 0) {
-            $this->day = 1;
-            $this->month = 1;
-            $this->year = 2008;
-        }
-        if ($arguments == 2) {
-            $this->day = $day;
-            $this->month = $month;
-            $this->year = 2008;
-        } else {
-            $this->day = $day;
-            $this->month = $month;
-            $this->year = $year;
-        }
+        $this->day = $day;
+        $this->month = $month;
+        $this->year = $year;
     }
 
     public function print()
