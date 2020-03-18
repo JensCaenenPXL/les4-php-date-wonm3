3af6a25f5fd2d2dfc5ca58b5d1a5b6f22dc8cde3
Jens Caenen <Jens.Caenen@student.pxl.be>
2020-03-18 14:21:35 +0100

    Startcode (Probeersel)

--- src/util/Date.php
+++ src/util/Date.php
@@ -1,1 +1,25 @@
 <?php
+namespace util;
+
+class Date
+{
+    private $day;
+    private $month;
+    private $year;
+
+    public function __construct($day, $month, $year)
+    {
+        $arguments = func_get_args();
+        if($arguments == null){
+            $this->day = 1;
+            $this->month = 1;
+            $this->year = 2008;
+        }
+
+        $this->day = $day;
+        $this->month = $month;
+        $this->year = $year;
+    }
+
+
+}
