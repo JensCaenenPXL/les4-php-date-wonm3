2a18eb333e8202e263a09331ab9ede57d2e43057
Jens Caenen <Jens.Caenen@student.pxl.be>
2020-03-18 17:21:16 +0100

    Created try/catch in verwerk.php

--- src/verwerk.php
+++ src/verwerk.php
@@ -2,18 +2,25 @@
 require_once '../vendor/autoload.php';
 use util\Date;
 
-$date1=Date::make($_GET["day"],$_GET["month"],$_GET["year"]);
+try {
+    $date1=Date::make($_GET["day"],$_GET["month"],$_GET["year"]);
 
-echo $date1->print();
-echo "of";
-echo $date1->printMonth();
+    echo $date1->print();
+    echo "of";
+    echo $date1->printMonth();
 
-echo "Als je 10 dagen, 2 maanden en 5 jaar toevoegt, dan krijg je de volgende datum:";
+    echo "Als je 10 dagen, 2 maanden en 5 jaar toevoegt, dan krijg je de volgende datum:";
 
-$date1->changeDay(10);
-$date1->changeMonth(2);
-$date1->changeYear(5);
+    $date1->changeDay(10);
+    $date1->changeMonth(2);
+    $date1->changeYear(5);
 
-echo $date1->print();
-echo "of";
-echo $date1->printMonth();
+    echo $date1->print();
+    echo "of";
+    echo $date1->printMonth();
+
+} catch (\Exception $dateException){
+    echo $dateException->getMessage();
+}
+
+
