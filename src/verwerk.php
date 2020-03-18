61030b1ea67d03ce6dc9ad6d357c7133a2ea4129
gristal69 <vincent.vandoninck@student.pxl.be>
2020-03-18 17:26:03 +0100

    resultaat opgeschoond

--- src/verwerk.php
+++ src/verwerk.php
@@ -6,17 +6,19 @@ try {
     $date1=Date::make($_GET["day"],$_GET["month"],$_GET["year"]);
 
     echo $date1->print();
-    echo "of";
+    echo " of ";
     echo $date1->printMonth();
+    echo "<br>";
 
     echo "Als je 10 dagen, 2 maanden en 5 jaar toevoegt, dan krijg je de volgende datum:";
 
     $date1->changeDay(10);
     $date1->changeMonth(2);
     $date1->changeYear(5);
+    echo "<br>";
 
     echo $date1->print();
-    echo "of";
+    echo " of ";
     echo $date1->printMonth();
 
 } catch (\Exception $dateException){
