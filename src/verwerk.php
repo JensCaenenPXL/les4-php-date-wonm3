7175dabccd77d0ed920c002fe940c7331e356ebb
Jens Caenen <Jens.Caenen@student.pxl.be>
2020-03-18 17:29:40 +0100

    Small invoer.html update

--- src/verwerk.php
+++ src/verwerk.php
@@ -5,22 +5,22 @@ use util\Date;
 try {
     $date1=Date::make($_GET["day"],$_GET["month"],$_GET["year"]);
 
-    echo $date1->print();
-    echo "of";
-    echo $date1->printMonth();
+    print $date1->print();
+    print " of ";
+    print $date1->printMonth();
 
-    echo "Als je 10 dagen, 2 maanden en 5 jaar toevoegt, dan krijg je de volgende datum:";
+    print "Als je 10 dagen, 2 maanden en 5 jaar toevoegt, dan krijg je de volgende datum:";
 
     $date1->changeDay(10);
     $date1->changeMonth(2);
     $date1->changeYear(5);
 
-    echo $date1->print();
-    echo "of";
-    echo $date1->printMonth();
+    print $date1->print();
+    print " of ";
+    print $date1->printMonth();
 
 } catch (\util\DateException $dateException){
-    echo $dateException->getMessage();
+    print $dateException->getMessage();
 }
 
 
