af258b1cd328ded618aa694d85c8bb83983515af
Jens Caenen <Jens.Caenen@student.pxl.be>
2020-03-18 17:04:59 +0100

    Minor changes to verwerk.php & invoer.html

--- src/verwerk.php
+++ src/verwerk.php
@@ -5,15 +5,15 @@ use util\Date;
 $date1=Date::make($_GET["day"],$_GET["month"],$_GET["year"]);
 
 echo $date1->print();
-echo "\nof\n";
+echo "of";
 echo $date1->printMonth();
 
-echo "Als je 10 dagen, 2 maanden en 5 jaar toevoegt, dan krijg je de volgende datum:\n";
+echo "Als je 10 dagen, 2 maanden en 5 jaar toevoegt, dan krijg je de volgende datum:";
 
 $date1->changeDay(10);
 $date1->changeMonth(2);
 $date1->changeYear(5);
 
 echo $date1->print();
-echo "\nof\n";
+echo "of";
 echo $date1->printMonth();
